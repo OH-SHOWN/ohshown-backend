87d68bbcab18a9935dd5909e4b27abc9d5400deb
dajing <wer321011@gmail.com>
2023-02-08 13:05:05 +0800

    feature(utils): add translate util

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -10,3 +10,9 @@ def set_function_attributes(**kwargs):
 
 def normalize_townname(townname):
     return townname.replace("台", "臺")
+
+def translate_array_of_integer(translate_dict, array_of_integer): 
+    if isinstance(array_of_integer, list):
+        return ", ".join(translate_dict.get(item, str(item)) for item in array_of_integer)
+
+    return ""
